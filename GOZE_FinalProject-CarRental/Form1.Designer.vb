f4ae26f71f12c7d8e332e54d2050da59a8f79cca
gwengz <gwengozesuperb@gmail.com>
2025-12-05 18:57:08 +0800

    all is working (add, update, delete, load)

--- GOZE_FinalProject-CarRental/Form1.Designer.vb
+++ GOZE_FinalProject-CarRental/Form1.Designer.vb
@@ -35,40 +35,42 @@ Partial Class Form1
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
         CType(Me.dgvRentals, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'txtCarModel
         '
-        Me.txtCarModel.Location = New System.Drawing.Point(125, 57)
+        Me.txtCarModel.Location = New System.Drawing.Point(106, 58)
         Me.txtCarModel.Name = "txtCarModel"
         Me.txtCarModel.Size = New System.Drawing.Size(100, 20)
         Me.txtCarModel.TabIndex = 0
         '
         'txtRenterName
         '
-        Me.txtRenterName.Location = New System.Drawing.Point(125, 83)
+        Me.txtRenterName.Location = New System.Drawing.Point(106, 84)
         Me.txtRenterName.Name = "txtRenterName"
         Me.txtRenterName.Size = New System.Drawing.Size(100, 20)
         Me.txtRenterName.TabIndex = 1
         '
         'dtpStartDate
         '
-        Me.dtpStartDate.Location = New System.Drawing.Point(125, 110)
+        Me.dtpStartDate.Location = New System.Drawing.Point(106, 111)
         Me.dtpStartDate.Name = "dtpStartDate"
         Me.dtpStartDate.Size = New System.Drawing.Size(200, 20)
         Me.dtpStartDate.TabIndex = 2
         '
         'dtpEndDate
         '
-        Me.dtpEndDate.Location = New System.Drawing.Point(125, 137)
+        Me.dtpEndDate.Location = New System.Drawing.Point(106, 138)
         Me.dtpEndDate.Name = "dtpEndDate"
         Me.dtpEndDate.Size = New System.Drawing.Size(200, 20)
         Me.dtpEndDate.TabIndex = 3
         '
         'btnAdd
         '
-        Me.btnAdd.Location = New System.Drawing.Point(125, 174)
+        Me.btnAdd.Location = New System.Drawing.Point(29, 168)
         Me.btnAdd.Name = "btnAdd"
         Me.btnAdd.Size = New System.Drawing.Size(75, 23)
         Me.btnAdd.TabIndex = 4
@@ -77,7 +79,7 @@ Partial Class Form1
         '
         'btnUpdate
         '
-        Me.btnUpdate.Location = New System.Drawing.Point(125, 203)
+        Me.btnUpdate.Location = New System.Drawing.Point(106, 168)
         Me.btnUpdate.Name = "btnUpdate"
         Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
         Me.btnUpdate.TabIndex = 5
@@ -86,7 +88,7 @@ Partial Class Form1
         '
         'btnDelete
         '
-        Me.btnDelete.Location = New System.Drawing.Point(125, 233)
+        Me.btnDelete.Location = New System.Drawing.Point(187, 168)
         Me.btnDelete.Name = "btnDelete"
         Me.btnDelete.Size = New System.Drawing.Size(75, 23)
         Me.btnDelete.TabIndex = 6
@@ -95,7 +97,7 @@ Partial Class Form1
         '
         'btnLoad
         '
-        Me.btnLoad.Location = New System.Drawing.Point(125, 263)
+        Me.btnLoad.Location = New System.Drawing.Point(268, 168)
         Me.btnLoad.Name = "btnLoad"
         Me.btnLoad.Size = New System.Drawing.Size(75, 23)
         Me.btnLoad.TabIndex = 7
@@ -105,15 +107,15 @@ Partial Class Form1
         'dgvRentals
         '
         Me.dgvRentals.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvRentals.Location = New System.Drawing.Point(226, 174)
+        Me.dgvRentals.Location = New System.Drawing.Point(12, 197)
         Me.dgvRentals.Name = "dgvRentals"
-        Me.dgvRentals.Size = New System.Drawing.Size(240, 150)
+        Me.dgvRentals.Size = New System.Drawing.Size(497, 150)
         Me.dgvRentals.TabIndex = 8
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(45, 64)
+        Me.Label1.Location = New System.Drawing.Point(26, 65)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(58, 13)
         Me.Label1.TabIndex = 9
@@ -122,7 +124,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(45, 90)
+        Me.Label2.Location = New System.Drawing.Point(26, 91)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(73, 13)
         Me.Label2.TabIndex = 10
@@ -131,7 +133,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(45, 116)
+        Me.Label3.Location = New System.Drawing.Point(26, 117)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(58, 13)
         Me.Label3.TabIndex = 11
@@ -140,17 +142,35 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(45, 143)
+        Me.Label4.Location = New System.Drawing.Point(26, 144)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(55, 13)
         Me.Label4.TabIndex = 12
         Me.Label4.Text = "End Date:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(26, 41)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(24, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "ID: "
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(106, 34)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(100, 20)
+        Me.txtID.TabIndex = 14
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
@@ -185,4 +205,6 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtID As TextBox
 End Class
--- GOZE_FinalProject-CarRental/Form1.vb
+++ GOZE_FinalProject-CarRental/Form1.vb
@@ -5,26 +5,116 @@ Public Class Form1
     Dim COMMAND As MySqlCommand
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
-        Dim query As String = "INSERT INTO `car_rental`.`car_rental` 
-                            (`ID`, `Car Model`, `Renter Name`, `Start Date`, `End Date`) 
-                            VALUES (@ID, @Car Model, @Renter Name, @Start Date, @End Date);"
+        Dim query As String = "INSERT INTO car_rental 
+                           (car_model, renter_name, start_date, end_date)
+                           VALUES (@car_model, @renter_name, @start_date, @end_date);"
+
         Try
-            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=car_rental;")
+            Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=car_rental;")
                 conn.Open()
                 Using cmd As New MySqlCommand(query, conn)
-                    cmd.Parameters.AddWithValue("Car Model", txtCarModel.Text)
-                    cmd.Parameters.AddWithValue("Renter Name", txtCarModel.Text)
-                    cmd.Parameters.AddWithValue("Start Date", CInt(txtCarModel.Text))
+
+                    cmd.Parameters.AddWithValue("@car_model", txtCarModel.Text)
+                    cmd.Parameters.AddWithValue("@renter_name", txtRenterName.Text)
+                    cmd.Parameters.AddWithValue("@start_date", dtpStartDate.Value.Date)
+                    cmd.Parameters.AddWithValue("@end_date", dtpEndDate.Value.Date)
+
                     cmd.ExecuteNonQuery()
-                    MessageBox.Show()
-                    MessageBox.Show("Record insert successful!")
-                    TextBoxName.Clear()
-                    TextBoxAge.Clear()
-                    TextBoxEmail.Clear()
+                    MessageBox.Show("Record added successfully!")
                 End Using
             End Using
+
+            LoadData()
+
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim query As String = "UPDATE car_rental SET 
+                           car_model=@car_model,
+                           renter_name=@renter_name,
+                           start_date=@start_date,
+                           end_date=@end_date
+                           WHERE id=@id;"
+
+        Try
+            Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=car_rental;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+
+                    cmd.Parameters.AddWithValue("@car_model", txtCarModel.Text)
+                    cmd.Parameters.AddWithValue("@renter_name", txtRenterName.Text)
+                    cmd.Parameters.AddWithValue("@start_date", dtpStartDate.Value.Date)
+                    cmd.Parameters.AddWithValue("@end_date", dtpEndDate.Value.Date)
+                    cmd.Parameters.AddWithValue("@id", CInt(txtID.Text))
+
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record updated successfully!")
+                End Using
+            End Using
+
+            LoadData()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim query As String = "DELETE FROM car_rental WHERE id=@id;"
+
+        Try
+            Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=car_rental;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+
+                    cmd.Parameters.AddWithValue("@id", CInt(txtID.Text))
+                    cmd.ExecuteNonQuery()
+
+                    MessageBox.Show("Record deleted successfully!")
+                End Using
+            End Using
+
+            LoadData()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        LoadData()
+    End Sub
+
+    Private Sub LoadData()
+        Dim query As String = "SELECT * FROM car_rental;"
+
+        Try
+            Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=car_rental;")
+                conn.Open()
+                Using da As New MySqlDataAdapter(query, conn)
+                    Dim dt As New DataTable
+                    da.Fill(dt)
+                    dgvRentals.DataSource = dt
+                End Using
+            End Using
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub dtgRentals_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRentals.CellClick
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow = dgvRentals.Rows(e.RowIndex)
+
+            txtID.Text = row.Cells("id").Value.ToString()
+            txtCarModel.Text = row.Cells("car_model").Value.ToString()
+            txtRenterName.Text = row.Cells("renter_name").Value.ToString()
+            dtpStartDate.Value = CDate(row.Cells("start_date").Value)
+            dtpEndDate.Value = CDate(row.Cells("end_date").Value)
+        End If
     End Sub
 End Class
